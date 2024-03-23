c69ea6e3996b297537079c8c3c81bdeeb2a01e1e
OwendB1 <owendebree@gmail.com>
2024-03-23 07:56:17 +0100

    Update storage GUID

--- src/Data/Scripts/ShipClassSystem/Settings.cs
+++ src/Data/Scripts/ShipClassSystem/Settings.cs
@@ -5,7 +5,7 @@
         public static readonly bool Debug = true;
 
         public static readonly int
-            LOG_LEVEL = 0; //messages with logPriority >= this will get logged, less than will be ignored
+            LOG_LEVEL = 1; //messages with logPriority >= this will get logged, less than will be ignored
 
         public static readonly int
             CLIENT_OUTPUT_LOG_LEVEL = 3; //messages with logPriority >= this will get output to clients
